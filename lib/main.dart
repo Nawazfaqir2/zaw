d129760f3f6b5b8e7aa4275405d785fac6094f00
Nawazfaqir2 <nawaz232196@gmail.com>
2023-08-28 17:06:56 +0500

    Screen Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tony_shop/flash.dart';
+
+import 'package:tony_shop/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-home: flash(),
+      home: SplashScreen(),
     );
   }
 }
