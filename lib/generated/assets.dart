60fef9e20696665a5b3f15d6c660d90511af167a
Nawazfaqir2 <nawaz232196@gmail.com>
2023-08-30 14:37:34 +0500

    czxdsd

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -9,10 +9,12 @@ class Assets {
   static const String animationsPage1 = 'animations/page1.json';
   static const String animationsPage2 = 'animations/page2.json';
   static const String imagesBac = 'images/bac.jpg';
+  static const String imagesBackgound = 'images/backgound.jpg';
   static const String imagesChild = 'images/child.jpg';
   static const String imagesHome = 'images/home.png';
   static const String imagesHow = 'images/how.jpg';
   static const String imagesKpk = 'images/kpk.png';
+  static const String imagesLo = 'images/lo.gif';
   static const String imagesLogo = 'images/logo.gif';
   static const String imagesThird = 'images/third.jpg';
   static const String tonyShopImagesLogo = 'images/logo.png';
